36e4ba711bf1bc05f2b36fd03d8394996b376d1f
Abhinav Gupta <93302416+abhinavmaddheshiya@users.noreply.github.com>
2023-10-11 23:53:32 +0530

    Add files via upload

---
+++ weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '27c83f1515msh04cb246f5900fcep1ff26djsn47ccd5d60c96',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e630bad7174ad88473c7ff168608a73b";
